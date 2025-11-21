6c89235a04eafde36b066b53c9b168f0d07d9065
MRfrend <Saimon.siipan@gmail.com>
2025-11-21 11:53:26 +0200

    Add Actor entity support and ActorsController
    Introduced ActorDTO with relevant properties and updated the DbContext to include the Actors DbSet. Implemented ActorsController with an Index action to display actors, and added a navigation link to the actors page in the shared layout.

--- Filminurk/Filminurk.Core/Dto/ActorDTO.cs
+++ Filminurk/Filminurk.Core/Dto/ActorDTO.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Filminurk.Core.Domain;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Filminurk.Core.Dto
 {
-    internal class ActorDTO
+    public class ActorDTO
     {
+        [Key]
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? NickName { get; set; }
+        public List<string>? MoviesActedFor { get; set; }
+        public Guid PortraitID { get; set; }
+        /* 3 enda mõeldud andmetüüpi */
+        public ActorSpecialization? PrimarySpecialization { get; set; }
+        public DateOnly? CareerStartYear { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
     }
 }
--- Filminurk/Filminurk/Controllers/ActorsController.cs
+++ Filminurk/Filminurk/Controllers/ActorsController.cs
@@ -1,6 +1,40 @@
-﻿namespace Filminurk.Controllers
+﻿using Filminurk.Data;
+using Filminurk.Data;
+using Filminurk.Models.Actors;
+using Filminurk.Models.Actors;
+using Filminurk.Models.UserComments;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Filminurk.Controllers
 {
-    public class ActorsController
+    public class ActorsController : Controller
     {
+        private readonly FilminurkTarpe24Context _context;
+        public ActorsController(FilminurkTarpe24Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+{
+
+    var result = _context.Actors
+        .Select(a => new ActorIndexViewModel
+        {
+            ActorID = a.ActorID,
+            FirstName = a.FirstName,
+            LastName = a.LastName,
+            NickName = a.NickName,
+            MoviesActedFor = a.MoviesActedFor,
+            PortraitID = a.PortraitID,
+            PrimarySpecialization = a.PrimarySpecialization,
+            CareerStartYear = a.CareerStartYear,
+            DateOfBirth = (DateOnly)a.DateOfBirth
+
+        }
+    );
+    return View(result);
+            }
     }
 }
